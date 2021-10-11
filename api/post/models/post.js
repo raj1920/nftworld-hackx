36c67b0b215d2fcdbe5b517f7a657a05efc4fa47
romitkarmakar <romitkarmakar@gmail.com>
2021-10-11 15:36:14 +0530

    Custom admin and algolia

--- api/post/models/post.js
+++ api/post/models/post.js
@@ -1,14 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
+const index = "nftworld";
+
 module.exports = {
-    lifecycles: {
-        async beforeCreate(data) {
-            await strapi.services.post.mintNFT(data.asset_link);
-        }
-    }
+  lifecycles: {
+    async beforeCreate(data) {
+      await strapi.services.post.mintNFT(data.asset_link);
+    },
+    async afterCreate(result, data) {
+      await strapi.services.algolia.saveObject(result, index);
+    },
+    async afterUpdate(result, params, data) {
+      await strapi.services.algolia.saveObject(result, index);
+    },
+    async afterDelete(result, params) {
+      await strapi.services.algolia.deleteObject(result.id, index);
+    },
+  },
 };
---
+++ config/hook.js
@@ -0,0 +1,10 @@
+module.exports = {
+  settings: {
+    algolia: {
+      enabled: true,
+      applicationId: "EFWGDX27Z7",
+      apiKey: "998985569b4ebcbe82e0cd2397264c09",
+      debug: true,
+    },
+  },
+};
